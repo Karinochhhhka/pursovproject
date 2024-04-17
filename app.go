78768c8b4dfbeb35710aeead46fb624a5f2166bd
Karinochhhhka <90684664+Karinochhhhka@users.noreply.github.com>
2024-04-17 15:27:24 +0300

    Update app.go

--- app.go
+++ app.go
@@ -1,42 +1,22 @@
-
 package main
 
 import (
 	"fmt"
 	"log"
+	"math/rand"
 	"net/http"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Hello, 世界")
+func randomHandler(w http.ResponseWriter, r *http.Request) {
+	randomNumber := rand.Intn(100)
+	fmt.Fprintf(w, "Random number: %d", randomNumber)
 }
 
 func main() {
-	http.HandleFunc("/", handler)
+	http.HandleFunc("/", randomHandler)
 	fmt.Println("Running demo app. Press Ctrl+C to exit...")
 	log.Fatal(http.ListenAndServe(":8888", nil))
 }
-// package main
-
-// import (
-// 	"fmt"
-// 	"log"
-// 	"math/rand"
-// 	"net/http"
-// 	//"time"
-// )
-
-// func randomHandler(w http.ResponseWriter, r *http.Request) {
-// 	randomNumber := rand.Intn(100)
-// 	fmt.Fprintf(w, "Random number: %d", randomNumber)
-// }
-
-// func main() {
-// 	http.HandleFunc("/", randomHandler)
-// 	fmt.Println("Running demo app. Press Ctrl+C to exit...")
-// 	log.Fatal(http.ListenAndServe(":8888", nil))
-// }
-
 // package main
 
 // import (
